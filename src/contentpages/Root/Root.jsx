9a472e3eac902ffd32c4430316d2013e192028f1
Henri Kähkönen <henri.kahkonen@gmail.com>
2024-05-22 15:23:46 +0300

    Adsense script testi osa 2

--- src/contentpages/Root/Root.jsx
+++ src/contentpages/Root/Root.jsx
@@ -16,11 +16,6 @@ export function Root() {
     return (
       
         <div className="App">
-
-          {/***/}
-          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8701862352371545"
-          crossorigin="anonymous"></script>
-          
           <div className="MainContainer">
             <Header/>
               <Outlet/>
